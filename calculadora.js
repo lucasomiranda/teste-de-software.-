161e4f5a803bebcfe21db2848b49c1c7bf5623cf
lucasomiranda <lucas.omiranda15@gmail.com>
2026-08-18 20:30:14 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,60 @@
-const mostra = document.getElementById("mostra");
+const display = document.getElementById("mostra");
 const ac = document.getElementById("ac");
 const raiz = document.getElementById("raiz");
 const parenteses = document.getElementById("parenteses");
-const divisao = document.getElementById("dividir");
-const sete = document.getElementById("sete");
-const oito = document.getElementById("oito");
-const nove = document.getElementById("nove");
-const vezes = document.getElementById("vezes");
-const quatro = document.getElementById("quatro");
-const cinco = document.getElementById("cinco");
-const seis = document.getElementById("seis");
-const menos = document.getElementById("menos");
-const um = document.getElementById("um");
-const dois = document.getElementById("dois");
-const tres = document.getElementById("tres");
-const mais = document.getElementById("mais");
-const zero = document.getElementById("zero");
 const virgula = document.getElementById("virgula");
 const delet = document.getElementById("delet");
 const igual = document.getElementById("igual");
 
+// operadores
+const divisao = document.getElementById("dividir");
+const menos = document.getElementById("menos");
+const vezes = document.getElementById("vezes");
+const mais = document.getElementById("mais");
+
+// numeral
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const zero = document.getElementById("zero");
 
 
+
+mais.addEventListener("click", () => {display.textContent += "+";});
+
+menos.addEventListener("click", () => {display.textContent += "-";});
+
+vezes.addEventListener("click", () => {display.textContent += "*";});
+
+divisao.addEventListener("click", () => {display.textContent += "/";});
+
+ac.addEventListener("click", () => {display.textContent = " ";})
+
+
+um.addEventListener("click", () => {display.textContent += "1";})
+dois.addEventListener("click", () => {display.textContent += "2";})
+tres.addEventListener("click", () => {display.textContent += "3";})
+quatro.addEventListener("click", () => {display.textContent += "4";})
+cinco.addEventListener("click", () => {display.textContent += "5";})
+seis.addEventListener("click", () => {display.textContent += "6";})
+sete.addEventListener("click", () => {display.textContent += "7";})
+oito.addEventListener("click", () => {display.textContent += "8";})
+nove.addEventListener("click", () => {display.textContent += "9";})
+
+
+
+igual.addEventListener("click", () => {
+try {
+    let resultado = eval(display.textContent)
+    display.textContent = resultado;
+} catch (error) {
+    display.textContent = "erro";
+}
+
+})
