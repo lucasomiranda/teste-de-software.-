edf13e72952342227b860ada9c991555cc6fc709
lucasomiranda <lucas.omiranda15@gmail.com>
2026-08-18 16:57:20 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+const mostra = document.getElementById("mostra");
+const ac = document.getElementById("ac");
+const raiz = document.getElementById("raiz");
+const parenteses = document.getElementById("parenteses");
+const divisao = document.getElementById("dividir");
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const vezes = document.getElementById("vezes");
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const menos = document.getElementById("menos");
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const mais = document.getElementById("mais");
+const zero = document.getElementById("zero");
+const virgula = document.getElementById("virgula");
+const delet = document.getElementById("delet");
+const igual = document.getElementById("igual");
+
+
+
